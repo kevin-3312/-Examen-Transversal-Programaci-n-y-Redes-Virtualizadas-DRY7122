eab5f1d8036ffd221a498023ba7541a04d300c33
NetAcad DEVASC <changeme@example.com>
2026-07-13 02:32:52 +0000

    Se agregan scripts Python del examen

---
+++ integrantes.py
@@ -0,0 +1,10 @@
+integrantes = [
+    "Kevin Lipin",
+    "Constanza Digmann",
+    "Israel Miranda"
+]
+
+print("Integrantes del grupo:")
+
+for integrante in integrantes:
+    print("-", integrante)
